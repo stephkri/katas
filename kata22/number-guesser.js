679ffc3ff4b203571350fb9900c1ee55e1567274
stephkri <stephane.krims@hotmail.ca>
2022-03-02 03:13:43 +0000

    Corectly make the number guesser with a recursive function

--- kata22/number-guesser.js
+++ kata22/number-guesser.js
@@ -1,6 +1,30 @@
 let prompt = require("prompt-sync")();
 
-let attempts = 0;
-let num = Math.floor(Math.random() * 100);
+let number = Math.floor(Math.random() * 100);
 
-// let guess = prompt("Guess a number:");
+const numberGuesser = (num, attempts) => {
+
+  let guess = parseFloat(prompt("Guess a number:"));
+
+  if (!/^\d+$/.test(guess)) {
+    console.log("Not a number! Try again.")
+    numberGuesser(num, attempts + 1)
+  }
+
+  if (guess > num) {
+    console.log("Too high!")
+    numberGuesser(num, attempts + 1)
+  }
+
+  if (guess < num) {
+    console.log("Too low!")
+    numberGuesser(num, attempts + 1)
+  }
+
+  if (guess === num) {
+    console.log(`Correct! You took ${attempts} attempts.`)
+    return
+  }
+}
+
+numberGuesser(number, 1)
