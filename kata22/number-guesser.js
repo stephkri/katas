2806ae77b3c682aeff02a3fd5b17b73fece5f7df
stephkri <stephane.krims@hotmail.ca>
2022-03-02 03:00:16 +0000

    Generate random number between 1 and 100

--- kata22/number-guesser.js
+++ kata22/number-guesser.js
@@ -1,7 +1,6 @@
 let prompt = require("prompt-sync")();
 
 let attempts = 0;
+let num = Math.floor(Math.random() * 100);
 
-const numberGuesser = () => {
-
-};
+// let guess = prompt("Guess a number:");
